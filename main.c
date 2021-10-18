3b96ada6bb2635017a3395ea784a61269744c829
Iumaloo <67984789+Iumaloo@users.noreply.github.com>
2021-10-17 23:56:39 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 #include "cat.h"
 #include "dog.h"
+#include "canary.h"
 
 int main(int argc, char const *argv[])
 {
 	Cat* hopkins = CAT_create("Hopkins");
 	Dog* alfred = DOG_create("Alfred");
+	Canary* tweety = CANARY_create("Tweety");
 
-	// Suscripción al evento SPEAK
+	// Suscripción al evento SPEAK por parte de ALFRED
 	alfred->smell(alfred, hopkins);
+    // Suscripción al evento SPEAK por parte de TWEETY
+	tweety->see(tweety,hopkins);
 
-
-	// Publicación del evento 
+	// Publicación del evento speak
 	hopkins->speak(hopkins);
 
 
 	hopkins->destroy(hopkins);
 	alfred->destroy(alfred);
+	tweety->destroy(tweety);
 
 	return EXIT_SUCCESS;
 }
